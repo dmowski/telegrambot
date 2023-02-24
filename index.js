3a4c8f518ca7444c83b54033319149ee5b0213bf
Dmowski Alexandr <dmowski@yandex.ru>
2023-02-24 04:46:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ const bot = new TelegramBot(process.env.BOT_API_TOKEN, {polling: true});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  console.log(msg);
-  
   bot.sendMessage(chatId, 'Я получил ваше сообщение!');
 });
